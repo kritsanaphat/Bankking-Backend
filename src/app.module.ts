eaec1a9579c70fd8569b25287ef63c98867b77ca
Kritsanaphat <63010040@kmitl.ac.th>
2022-10-22 22:35:52 +0700

    localhost to host

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,7 +27,7 @@ import { ActivityTransaction } from './activity-transaction/entities/activity-tr
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: 'host',
       port: 5432,
       username: 'postgres1',
       password: 'kritsanaphat1',
